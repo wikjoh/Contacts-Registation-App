c05098c6cb37725c9e6932738588ae2be5672582
wikai <80284758+wikai@users.noreply.github.com>
2024-12-16 16:19:56 +0100

    Add project files.

---
+++ Presentation.Console_MainApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Presentation.Console_MainApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
