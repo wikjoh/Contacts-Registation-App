498d4486178d12c3cee370b381b0c5e544587f05
wikai <80284758+wikai@users.noreply.github.com>
2024-12-17 14:42:43 +0100

    Add ContactFileService and its interface

---
+++ Business/Services/ContactFileService.cs
@@ -0,0 +1,10 @@
+﻿using Business.Interfaces;
+
+namespace Business.Services;
+
+public class ContactFileService : FileService, IContactFileService
+{
+    public ContactFileService(string directoryPath, string fileName) : base(directoryPath, fileName)
+    {
+    }
+}
