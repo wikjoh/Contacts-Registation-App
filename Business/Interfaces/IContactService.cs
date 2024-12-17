7fa19494c5faaf41414c861a4d702149b95ddd84
wikai <80284758+wikai@users.noreply.github.com>
2024-12-17 14:20:06 +0100

    Add interfaces for FileService and ContactService

---
+++ Business/Interfaces/IContactService.cs
@@ -0,0 +1,10 @@
+﻿using Business.Dtos;
+using Business.Models;
+
+namespace Business.Interfaces;
+
+public interface IContactService
+{
+    bool CreateContact(ContactDto contactForm);
+    IEnumerable<ContactModel> GetContacts();
+}
---
+++ Business/Interfaces/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace Business.Interfaces;
+
+public interface IFileService
+{
+    bool SaveToFile(string content);
+    string ReadFromFile();
+}
