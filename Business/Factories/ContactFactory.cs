f0f9f5f19d290f7f341b560fc9258f17887a45e5
wikai <80284758+wikai@users.noreply.github.com>
2024-12-17 20:07:08 +0100

    Add ContactFactory

---
+++ Business/Factories/ContactFactory.cs
@@ -0,0 +1,12 @@
+﻿using Business.Dtos;
+
+namespace Business.Factories;
+
+public static class ContactFactory
+{
+    public static ContactDto Create()
+    {
+        return new ContactDto();
+    }
+
+}
