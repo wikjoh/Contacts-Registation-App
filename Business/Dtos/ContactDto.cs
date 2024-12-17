d8d6c5e8498f2d4746a1f4b9c2c6a6c2c1302527
wikai <80284758+wikai@users.noreply.github.com>
2024-12-17 13:20:52 +0100

    Add ContactDto and ContactModel

---
+++ Business/Dtos/ContactDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Business.Dtos;
+
+public class ContactDto
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string StreetAddress { get; set; } = null!;
+    public int PostalCode { get; set; }
+    public string City { get; set; } = null!;
+}
---
+++ Business/Models/ContactModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Business.Models;
+
+public class ContactModel
+{
+    public int Id { get; set; }
+    public Guid Guid { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string StreetAddress { get; set; } = null!;
+    public int PostalCode { get; set; }
+    public string City { get; set; } = null!;
+}
