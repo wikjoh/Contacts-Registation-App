44ec68c81f6796905150a703db1208ef3d439e6a
wikai <80284758+wikai@users.noreply.github.com>
2025-01-09 11:49:43 +0100

    Add Xunittests for InputValidator

---
+++ Business.Tests/Helpers/InputValidator_Tests.cs
@@ -0,0 +1,35 @@
+﻿using Business.Dtos;
+using Business.Helpers;
+
+namespace Business.Tests.Helpers;
+
+public class InputValidator_Tests
+{
+    [Fact]
+    public void Validate_ShouldReturnNull_OnValidationSuccess()
+    {
+        // Arrange
+        var input = "ValidFirstName";
+        var propertyName = nameof(ContactDto.FirstName);
+
+        // Act
+        var results = InputValidator.Validate(input, propertyName);
+
+        // Assert
+        Assert.Null(results);
+    }
+
+    [Fact]
+    public void Validate_ShouldReturnValidationResults_OnValidationFailure()
+    {
+        // Arrange
+        var input = "X"; //Invalid FirstName, MinLength(2)
+        var propertyName = nameof(ContactDto.FirstName);
+        // Act
+        var results = InputValidator.Validate(input, propertyName);
+
+        // Assert
+        Assert.NotNull(results);
+        Assert.Equal("First name must be at least 2 characters", results![0].ErrorMessage);
+    }
+}
