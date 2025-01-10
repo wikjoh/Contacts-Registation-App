294a46dd864d826ae5684cb8e96bd18123c82588
wikai <80284758+wikai@users.noreply.github.com>
2025-01-10 16:02:33 +0100

    Add ContactService tests with mocking

--- Business.Tests/Services/ContactService_Tests.cs
+++ Business.Tests/Services/ContactService_Tests.cs
@@ -1,4 +1,5 @@
-﻿using Business.Interfaces;
+﻿using Business.Dtos;
+using Business.Interfaces;
 using Business.Models;
 using Business.Services;
 using Moq;
@@ -18,6 +19,34 @@ public class ContactService_Tests
     }
 
     [Fact]
+    public void GetSampleContacts_ShouldReturnListWithSampleContact()
+    {
+        // arrange
+        List<ContactModel> sampleContactList = [
+            new ContactModel
+            {
+                Id = 1,
+                Guid = new Guid("4b25d304-d5de-475f-8445-a960402bae65"),
+                FirstName = "Sample",
+                LastName = "Contact",
+                Email = "sample.contact@domain.com",
+                PhoneNumber = "0700123456",
+                StreetAddress = "Sample Street 1",
+                PostalCode = 12345,
+                City = "Sample City"
+            }
+        ];
+
+        // act
+        var result = _contactService.GetSampleContacts();
+
+        // assert
+        // Can't use Assert.Equal on lists directly since they are two separate lists with different references. Serialize to json and compare strings instead.
+        Assert.Equal(JsonSerializer.Serialize(result), JsonSerializer.Serialize(sampleContactList));
+    }
+
+
+    [Fact]
     public void GetContacts_ShouldReturnListWithSampleContact_IfContactsFileDoesNotExist()
     {
         // arrange
@@ -29,7 +58,106 @@ public class ContactService_Tests
         var result = _contactService.GetContacts();
 
         // assert
-            // Can't use Assert.Equal on lists directly since they are two separate lists with different references. Serialize to json and compare strings instead.
+        // Can't use Assert.Equal on lists directly since they are two separate lists with different references. Serialize to json and compare strings instead.
         Assert.Equal(JsonSerializer.Serialize(result), JsonSerializer.Serialize(_contactService.GetSampleContacts()));
+    }
+
+
+    [Fact]
+    public void GetContacts_ShouldReturnListOfContacts_IfContactsFileContainsContacts()
+    {
+        // arrange
+        List<ContactModel> mockedContactsListFromFile = [
+            new ContactModel
+            {
+                Id = 1,
+                Guid = new Guid("4b25d304-d5de-475f-8445-a960402bae65"),
+                FirstName = "Sample",
+                LastName = "Contact",
+                Email = "sample.contact@domain.com",
+                PhoneNumber = "0700123456",
+                StreetAddress = "Sample Street 1",
+                PostalCode = 12345,
+                City = "Sample City"
+            },
+            new ContactModel
+            {
+                Id = 2,
+                Guid = new Guid("4b25d304-d5de-475f-8445-a960402bae66"),
+                FirstName = "Sample2",
+                LastName = "Contact2",
+                Email = "sample.contact@domain.com",
+                PhoneNumber = "0700123456",
+                StreetAddress = "Sample Street 1",
+                PostalCode = 12345,
+                City = "Sample City"
+            }
+        ];
+
+        _contactRepositoryMock
+            .Setup(cr => cr.ReadFromFile())
+            .Returns(mockedContactsListFromFile);
+
+        // act
+        var result = _contactService.GetContacts();
+
+        // assert
+        // Can't use Assert.Equal on lists directly since they are two separate lists with different references. Serialize to json and compare strings instead.
+        Assert.Equal(JsonSerializer.Serialize(result), JsonSerializer.Serialize(mockedContactsListFromFile));
+    }
+
+
+    [Fact]
+    public void GetContacts_ShouldReturnEmptyList_IfContactsFileIsEmpty()
+    {
+        // arrange
+        List<ContactModel> mockedContactsListFromFile = [];
+
+        _contactRepositoryMock
+            .Setup(cr => cr.ReadFromFile())
+            .Returns(mockedContactsListFromFile);
+
+        // act
+        var result = _contactService.GetContacts();
+
+        // assert
+        Assert.Equal(result, []);
+    }
+
+
+    [Fact]
+    public void CreateContact_ShouldReturnTrue_WhenContactIsCreatedSuccessfully()
+    {
+        // arrange
+        ContactModel sampleContact = _contactService.GetContacts().First();
+        ContactDto sampleDto = new()
+        {
+            FirstName = sampleContact.FirstName,
+            LastName = sampleContact.LastName,
+            Email = sampleContact.Email,
+            PhoneNumber = sampleContact.PhoneNumber,
+            StreetAddress = sampleContact.StreetAddress,
+            PostalCode = sampleContact.PostalCode,
+            City = sampleContact.City
+        };
+
+        // act
+        var result = _contactService.CreateContact(sampleDto);
+
+        // assert
+        Assert.True(result);
+    }
+
+
+    [Fact]
+    public void CreateContact_ShouldReturnFalse_WhenSuppliedParameterIsNull()
+    {
+        // arrange
+
+        // act
+        var result = _contactService.CreateContact(null);
+
+        // assert
+        Assert.False(result);
     }
 }
